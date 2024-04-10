09582d18388740602d99756dffb58a863e04b736
Leen151 <leen.m@orange.fr>
2024-04-10 17:47:37 +0200

    Ajout de react router + creation des pages, installation de Sass
    (+ nettoyage dans les fichiers css)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import './App.css'
+import './style.scss'
+import { Route, Routes } from "react-router-dom";
+import { Home } from './pages/home/Home'
+import { ErrorPage } from './pages/errorPage/ErrorPage'
+import { AboutPage } from './pages/aboutPage/AboutPage';
+import { Header } from './components/header/Header';
+import { Footer } from './components/footer/Footer';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <p>Import composant</p>
-    </div>
-  );
+    <>
+      <Header />
+      <Routes>      
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<AboutPage/>} />
+        <Route path='*' element={<ErrorPage/>}/>
+      </Routes>
+      <Footer />
+    </>
+  )  
 }
   
-  export default App;
+export default App;
