ee075f9d551a986dd1e473c0dabc5ad13372f8f0
Leen151 <leen.m@orange.fr>
2024-04-21 09:31:51 +0200

    collapse : ajout icone, css et animations

--- src/components/collapse/Collapse.jsx
+++ src/components/collapse/Collapse.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import './collapse.scss'
+import Chevron from '../../assets/chevron.svg'
 
 export const Collapse = ({props}) => {
     const [unfolded, setUnfolded] = useState(false)
@@ -9,9 +10,9 @@ export const Collapse = ({props}) => {
     }
 
     return (
-    <div className="collapse">    
-        <h3 className="collapse-title" onClick={toggle}>{props.title} </h3>
+    <div className="collapse">
+        <h3 className="collapse-title" onClick={toggle}>{props.title} <img src={Chevron} className={`chevron${unfolded ? " active" : ""}`}/></h3>
         <div className={`collapse-text${unfolded ? " active" : ""}`}>{props.content}</div>
     </div>
-)
+    )
 }
